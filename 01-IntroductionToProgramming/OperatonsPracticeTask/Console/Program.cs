33499e2157fe78592301b23fcc45c07428593d94
hudsonengineer <hudsonsouza@tutanota.com>
2026-02-18 22:18:08 -0300

    #02 update Operatorns Practice Task (Operations)

--- 01-IntroductionToProgramming/OperatonsPracticeTask/Console/Program.cs
+++ 01-IntroductionToProgramming/OperatonsPracticeTask/Console/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OpeatornsPraticeTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Hudson";
+            string SecondName = "Ferraz";
+            string LastName = "Souza";
+            string FullName = $"{FirstName} {SecondName} {LastName}";
+            char Gender = 'M';
+            int Age = 26;
+            DateTime BirthDate = new DateTime(1999, 10, 30);
+            string CurrentJob = "Architectural Hardware House";
+            float Height = 1.78f;
+            string EyeColour = "Brown";
+
+            Console.WriteLine($"{FirstName}"); // a.
+            Console.WriteLine($"{SecondName}"); // b.
+            Console.WriteLine($"{LastName}"); // c.
+            Console.WriteLine($"{FullName}"); // d.
+            Console.WriteLine($"{Gender}"); // e.
+            Console.WriteLine($"{Age}"); // f.
+            Console.WriteLine(BirthDate); // g.
+            Console.WriteLine($"{CurrentJob}"); // h.
+            Console.WriteLine($"{Height} Meters"); // i.
+            Console.WriteLine($"{EyeColour}"); // j.
+        }
+    }
+}
