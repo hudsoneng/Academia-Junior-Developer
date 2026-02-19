ff37da5a9a5a757a7535c10733bfe740f86b2d4f
hudsonengineer <hudsonsouza@tutanota.com>
2026-02-18 22:26:21 -0300

    #03: fix .gitignore to clean

---
+++ 01-IntroductionToProgramming/OperatonsPracticeTask/Operations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a 1st number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a 2nd number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a 3rd number: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a 4th number: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            // primitive data types
+            int GreatestNumber = a;
+
+            if (b > GreatestNumber)
+                GreatestNumber = b;
+
+            if (c > GreatestNumber)
+                GreatestNumber = c;
+
+            if (d > GreatestNumber)
+                GreatestNumber = d;
+
+            Console.WriteLine($"The greatest number is: {GreatestNumber}");
+        }
+    }
+}
